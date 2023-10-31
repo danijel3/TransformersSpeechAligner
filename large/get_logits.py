d70a2507f6c05e468f150be94e33f0a5b2d9da3e
danijel3 <danijel@pja.edu.pl>
2023-10-31 18:37:19 +0100

    Cleanup cache

--- large/get_logits.py
+++ large/get_logits.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import numpy as np
 import torch
 import torchaudio
-from datasets import Dataset
+from datasets import Dataset, disable_caching
 from transformers import AutoProcessor, AutoModelForCTC
 from transformers.pipelines.automatic_speech_recognition import chunk_iter
 
@@ -39,7 +39,9 @@ def get_logits(audio_file: Path, model_name: str, chunk_len: float = 10, left_st
                        'input_values': chunk['input_values'][0],
                        'attention_mask': chunk['attention_mask'][0]}
 
-    data = Dataset.from_generator(chunking)
+    chunks = Dataset.from_generator(chunking)
+
+    data.cleanup_cache_files()
 
     logger.info(f'Split file into {data.num_rows} chunks.')
 
@@ -54,7 +56,9 @@ def get_logits(audio_file: Path, model_name: str, chunk_len: float = 10, left_st
     s = time.perf_counter()
 
     with torch.no_grad():
-        logits = data.map(process_logits, batched=True, batch_size=batch_size)
+        logits = chunks.map(process_logits, batched=True, batch_size=batch_size)
+
+    chunks.cleanup_cache_files()
 
     dur = time.perf_counter() - s
     logger.info(f'Took {dur:.2f}s which is {dur / audio_dur:.2f}x realtime.')
@@ -73,6 +77,8 @@ def get_logits(audio_file: Path, model_name: str, chunk_len: float = 10, left_st
         right_n = token_n - right
         full.append(np.array(lit)[left:right_n, :])
 
+    logits.cleanup_cache_files()
+
     return np.vstack(full)
 
 
@@ -88,6 +94,8 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=8)
 
     args = parser.parse_args()
+
+    disable_caching()
 
     logits = get_logits(args.audio_file, args.model_name, args.chunk_len, args.left_stride, args.right_stride,
                         args.device, args.batch_size)
