a6822fb7dafbd8cb798b0285b2be04eb89a21f6e
danijel3 <danijel@pja.edu.pl>
2023-11-15 20:21:02 +0100

    Added better index

--- visualize/visualize.py
+++ visualize/visualize.py
@@ -2,10 +2,13 @@ import argparse
 import json
 import re
 from pathlib import Path
+from typing import List, Dict
 
-from flask import Flask, render_template, make_response, request, abort
+from flask import Flask, render_template, make_response, request, abort, jsonify, redirect
 
 app = Flask('visualize')
+
+index_file = Path('index.json')
 
 audio_dir = Path('debug-croatian/wav')
 json_dir = Path('debug-croatian/output')
@@ -13,22 +16,16 @@ json_dir = Path('debug-croatian/output')
 
 @app.route('/')
 def index():
-    files = sorted([x.stem for x in json_dir.glob('*.json')])
+    files = []
 
-    return render_template('index.html', files=files)
+    if index_file.exists():
+        with open(index_file) as f:
+            files = json.load(f)
+
+    return render_template('index.html', files=files, audio_dir=audio_dir, json_dir=json_dir)
 
 
-@app.route('/visualize/<utt>')
-def visualize(utt):
-    audio_file = audio_dir / (utt + '.wav')
-    json_file = json_dir / (utt + '.json')
-
-    if not audio_file.exists() or not json_file.exists():
-        abort(404)
-
-    with open(json_file) as f:
-        annotations = json.load(f)
-
+def get_stats(annotations: List) -> Dict:
     stats = {'ref_only_seg': 0, 'ref_only_words': 0,
              'reco_only_seg': 0, 'reco_only_words': 0,
              'ok_match_seg': 0, 'ok_match_words': 0}
@@ -50,6 +47,53 @@ def visualize(utt):
             num += seg['errors']['num']
 
     stats['overall_wer'] = round(100 * (num - corr) / num, 2)
+    return stats
+
+
+@app.route('/reindex', methods=['POST'])
+def reindex():
+    global audio_dir, json_dir
+
+    if 'audio_dir' in request.args:
+        audio_dir = Path(request.args['audio_dir'])
+    if 'json_dir' in request.args:
+        json_dir = Path(request.args['json_dir'])
+
+    utt_to_date = {}
+    if Path('utt_to_date.json').exists():
+        with open('utt_to_date.json') as f:
+            utt_to_date = json.load(f)
+
+    files = []
+    for json_f in json_dir.glob('*.json'):
+        utt = {'utt': json_f.stem}
+        if utt['utt'] in utt_to_date:
+            utt['date'] = utt_to_date[utt['utt']]
+        else:
+            utt['date'] = ''
+        with open(json_f) as f:
+            annotations = json.load(f)
+        utt.update(get_stats(annotations))
+        files.append(utt)
+
+    with open(index_file, 'w') as f:
+        json.dump(files, f, indent=4)
+
+    return redirect('/')
+
+
+@app.route('/visualize/<utt>')
+def visualize(utt):
+    audio_file = audio_dir / (utt + '.wav')
+    json_file = json_dir / (utt + '.json')
+
+    if not audio_file.exists() or not json_file.exists():
+        abort(404)
+
+    with open(json_file) as f:
+        annotations = json.load(f)
+
+    stats = get_stats(annotations)
 
     annot_filt = annotations
     if 'noref' in request.args:
